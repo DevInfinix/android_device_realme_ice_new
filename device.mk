279f55eccbefb0f0cbca1c0010d8fc4d72778d75
DevInfinix <contact.devinfinix@gmail.com>
2024-09-25 23:02:20 +0530

    ice: Define platform flags

--- device.mk
+++ device.mk
@@ -8,6 +8,10 @@
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := lahaina
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info_yupikidp.xml:$(TARGET_COPY_OUT_ODM)/etc/audio_platform_info.xml \
