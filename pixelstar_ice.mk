31800a9d9a7e4672442e3b92bbe852720a91f635
DevInfinix <singhsamarjeet2007@gmail.com>
2024-10-03 03:19:56 +0530

    ice: Inherit common PixelStar config

--- pixelstar_ice.mk
+++ pixelstar_ice.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from ice device
 $(call inherit-product, device/realme/ice/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixelstar stuff.
+$(call inherit-product, vendor/pixelstar/config/common_full_phone.mk)
 
 PRODUCT_NAME := pixelstar_ice
 PRODUCT_DEVICE := ice
