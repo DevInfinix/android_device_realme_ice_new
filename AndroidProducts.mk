e15a2c5e68e130d75a4e1b2229949f4742451cbc
INFINIX <72700816+DevInfinix@users.noreply.github.com>
2024-10-27 14:17:16 +0530

    ice: Fixup: Bringup for everest

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_ice.mk
+    $(LOCAL_DIR)/everest_ice.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_ice-user \
-    lineage_ice-userdebug \
-    lineage_ice-eng
+    everest_ice-user \
+    everest_ice-userdebug \
+    everest_ice-eng
