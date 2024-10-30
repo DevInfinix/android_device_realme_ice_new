36200de6fe73d0ee7e043146dacc9e89fa3ab559
INFINIX <72700816+DevInfinix@users.noreply.github.com>
2024-10-30 13:45:37 +0530

    ice: Prepare for official builds
    *There's no need to explicitly include keys for signing official build.
    *Update build props to include stock fingerprint
    
    Signed-off-by: DevInfinix <singhsamarjeet2007@gmail.com>

--- pixelage_ice.mk
+++ pixelage_ice.mk
@@ -26,13 +26,13 @@ PRODUCT_SYSTEM_DEVICE := RE54BFL1
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 # Pixelage Flags
-WITH_GMS := false
+PIXELAGE_BUILDTYPE := OFFICIAL
+WITH_GMS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 PRODUCT_NO_CAMERA := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc=$(call normalize-path-list, "RMX3461-user 13 TP1A.220905.001 R.1119f23-59b8-59b9 release-keys") \
     DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
-    DeviceProduct=$(PRODUCT_SYSTEM_NAME)
-
-BUILD_FINGERPRINT := realme/RMX3461/RE54BFL1:13/TP1A.220905.001/R.1119f23-59b8-59b9:user/release-keys
+    DeviceProduct=$(PRODUCT_SYSTEM_NAME) \
+    BuildFingerprint := realme/RMX3461/RE54BFL1:13/TP1A.220905.001/R.1119f23-59b8-59b9:user/release-keys
